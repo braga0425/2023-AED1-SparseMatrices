1b42ae5e41e17b5d7ebe1ea95b378eb0184ff3e9
Leonardo Braga <braga0425@gmail.com>
2023-08-27 20:45:44 -0300

    Some fixes.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Estrutura da célula da matriz
 struct matrix {
     struct matrix* right;
     struct matrix* below;
@@ -9,351 +8,145 @@ struct matrix {
     int column;
     float info;
 };
+
 typedef struct matrix Matrix;
 
-// Função para criar uma matriz esparsa
-Matrix* matrix_create() {
-    int m, n;
-    scanf("%d %d", &m, &n);
-    
+Matrix* matrix_create(void) {
     Matrix* head = (Matrix*)malloc(sizeof(Matrix));
-    head->line = -1;
-    head->column = -1;
-    head->info = 0;
+    if (head == NULL) {
+        perror("Erro na alocação de memória");
+        exit(EXIT_FAILURE);
+    }
+    
     head->right = head;
     head->below = head;
-    
-    Matrix* current_row = head;
-    Matrix* previous_in_column[n];
-    
-    for (int j = 0; j < n; j++) {
-        previous_in_column[j] = head;
-    }
-    
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            float value;
-            scanf("%f", &value);
-            if (value != 0) {
-                Matrix* new_cell = (Matrix*)malloc(sizeof(Matrix));
-                new_cell->line = i;
-                new_cell->column = j;
-                new_cell->info = value;
-                
-                // Conectar na linha
-                current_row->right = new_cell;
-                new_cell->right = head;
-                current_row = new_cell;
-                
-                // Conectar na coluna
-                new_cell->below = previous_in_column[j]->below;
-                previous_in_column[j]->below = new_cell;
-                previous_in_column[j] = new_cell;
-            }
-        }
-    }
+    head->line = -1; // Cabeça da linha
+    head->column = -1; // Cabeça da coluna
+    head->info = 0.0; // Valor padrão para a célula cabeça
     
     return head;
 }
 
-// Função para destruir a matriz e liberar memória
-void matrix_destroy(Matrix* m) {
-    Matrix* current = m->right;
-    while (current != m) {
-        Matrix* temp = current;
-        current = current->right;
-        free(temp);
-    }
-    free(m);
-}
-
-// Função para imprimir a matriz
-void matrix_print(Matrix* m) {
-    Matrix* current_row = m->below;
-    while (current_row != m) {
-        Matrix* current_cell = current_row->right;
-        while (current_cell != current_row) {
-            printf("%d %d %.1f\n", current_cell->line, current_cell->column, current_cell->info);
-            current_cell = current_cell->right;
-        }
-        current_row = current_row->below;
-    }
-}
-
-// Função para somar duas matrizes
-Matrix* matrix_add(Matrix* m, Matrix* n) {
-    // Verificar se as matrizes têm o mesmo tamanho
-    if (m->line != n->line || m->column != n->column) {
-        fprintf(stderr, "Erro: Matrizes de tamanhos diferentes.\n");
-        return NULL;
-    }
-
-    int numRows = m->line;
-    int numCols = m->column;
-
-    // Criar uma matriz resultante com o mesmo tamanho
-    Matrix* result = (Matrix*)malloc(sizeof(Matrix));
-    result->line = numRows;
-    result->column = numCols;
-    result->info = 0;
-    result->right = result;
-    result->below = result;
-
-    // Arrays para rastrear a cabeça de linha e coluna na matriz resultante
-    Matrix* headRows[numRows];
-    Matrix* headCols[numCols];
-
-    // Inicializar cabeças de linha
-    for (int i = 0; i < numRows; i++) {
-        headRows[i] = result;
-    }
-
-    // Inicializar cabeças de coluna
-    for (int j = 0; j < numCols; j++) {
-        headCols[j] = result;
-    }
-
-    // Percorrer a matriz m e adicionar elementos à matriz resultante
-    Matrix* current_m = m->below;
-    while (current_m != m) {
-        Matrix* current_cell_m = current_m->right;
-        while (current_cell_m != current_m) {
-            int i = current_cell_m->line;
-            int j = current_cell_m->column;
-            float value_m = current_cell_m->info;
-
-            // Adicionar o elemento de m à matriz resultante
-            Matrix* new_cell = (Matrix*)malloc(sizeof(Matrix));
-            new_cell->line = i;
-            new_cell->column = j;
-            new_cell->info = value_m;
-
-            // Conectar na linha
-            new_cell->right = result;
-            new_cell->below = headRows[i]->below;
-            headRows[i]->below = new_cell;
-            headRows[i] = new_cell;
-
-            // Conectar na coluna
-            new_cell->below = headCols[j]->below;
-            headCols[j]->below = new_cell;
-            headCols[j] = new_cell;
-
-            current_cell_m = current_cell_m->right;
-        }
-        current_m = current_m->below;
-    }
-
-    return result;
-}
-
-// Função para multiplicar duas matrizes
-Matrix* matrix_multiply(Matrix* m, Matrix* n) {
-    // Verificar se as matrizes podem ser multiplicadas
-    if (m->column != n->line) {
-        fprintf(stderr, "Erro: Matrizes não podem ser multiplicadas.\n");
-        return NULL;
-    }
-
-    int numRows_m = m->line;
-    int numCols_m = m->column;
-    int numRows_n = n->line;
-    int numCols_n = n->column;
-
-    // Criar uma matriz resultante com o tamanho apropriado
-    Matrix* result = (Matrix*)malloc(sizeof(Matrix));
-    result->line = numRows_m;
-    result->column = numCols_n;
-    result->info = 0;
-    result->right = result;
-    result->below = result;
-
-    // Arrays para rastrear a cabeça de linha e coluna na matriz resultante
-    Matrix* headRows[numRows_m];
-    Matrix* headCols[numCols_n];
-
-    // Inicializar cabeças de linha
-    for (int i = 0; i < numRows_m; i++) {
-        headRows[i] = result;
-    }
-
-    // Inicializar cabeças de coluna
-    for (int j = 0; j < numCols_n; j++) {
-        headCols[j] = result;
-    }
-
-    // Realizar a multiplicação
-    for (int i = 0; i < numRows_m; i++) {
-        for (int j = 0; j < numCols_n; j++) {
-            float dot_product = 0.0;
-            for (int k = 0; k < numCols_m; k++) {
-                // Encontrar elementos relevantes nas matrizes m e n
-                Matrix* current_m = m->below;
-                while (current_m->line != i) {
-                    current_m = current_m->below;
-                }
-                Matrix* current_n = n->right;
-                while (current_n->column != j) {
-                    current_n = current_n->right;
-                }
-
-                // Multiplicar e somar ao produto escalar
-                dot_product += current_m->info * current_n->info;
-            }
-
-            // Se o produto escalar for diferente de zero, adicionar à matriz resultante
-            if (dot_product != 0) {
-                // Adicionar o elemento resultante à matriz resultante
-                Matrix* new_cell = (Matrix*)malloc(sizeof(Matrix));
-                new_cell->line = i;
-                new_cell->column = j;
-                new_cell->info = dot_product;
-
-                // Conectar na linha
-                new_cell->right = result;
-                new_cell->below = headRows[i]->below;
-                headRows[i]->below = new_cell;
-                headRows[i] = new_cell;
-
-                // Conectar na coluna
-                new_cell->below = headCols[j]->below;
-                headCols[j]->below = new_cell;
-                headCols[j] = new_cell;
-            }
-        }
-    }
-
-    return result;
-}
-
-// Função para calcular a transposta de uma matriz
-Matrix* matrix_transpose(Matrix* m) {
-    int numRows = m->line;
-    int numCols = m->column;
-
-    // Criar uma matriz resultante com linhas e colunas invertidas
-    Matrix* result = (Matrix*)malloc(sizeof(Matrix));
-    result->line = numCols;
-    result->column = numRows;
-    result->info = 0;
-    result->right = result;
-    result->below = result;
-
-    // Arrays para rastrear a cabeça de linha e coluna na matriz resultante
-    Matrix* headRows[numRows];
-    Matrix* headCols[numCols];
-
-    // Inicializar cabeças de linha
-    for (int i = 0; i < numRows; i++) {
-        headRows[i] = result;
-    }
-
-    // Inicializar cabeças de coluna
-    for (int j = 0; j < numCols; j++) {
-        headCols[j] = result;
-    }
-
-    // Percorrer a matriz original e criar a transposta
-    Matrix* current_m = m->below;
-    while (current_m != m) {
-        Matrix* current_cell_m = current_m->right;
-        while (current_cell_m != current_m) {
-            int i = current_cell_m->line;
-            int j = current_cell_m->column;
-            float value_m = current_cell_m->info;
-
-            // Adicionar o elemento da matriz original à transposta
-            Matrix* new_cell = (Matrix*)malloc(sizeof(Matrix));
-            new_cell->line = j; // Trocar linhas e colunas
-            new_cell->column = i;
-            new_cell->info = value_m;
-
-            // Conectar na linha
-            new_cell->right = result;
-            new_cell->below = headRows[j]->below;
-            headRows[j]->below = new_cell;
-            headRows[j] = new_cell;
-
-            // Conectar na coluna
-            new_cell->below = headCols[i]->below;
-            headCols[i]->below = new_cell;
-            headCols[i] = new_cell;
-
-            current_cell_m = current_cell_m->right;
-        }
-        current_m = current_m->below;
-    }
-
-    return result;
-}
-
-// Função para obter o valor de um elemento na matriz
 float matrix_getelem(Matrix* m, int x, int y) {
-    // Percorrer a matriz para encontrar o elemento desejado
-    Matrix* current = m->below;
-    while (current != m) {
-        Matrix* current_cell = current->right;
-        while (current_cell != current) {
-            if (current_cell->line == x && current_cell->column == y) {
-                return current_cell->info; // Elemento encontrado
+    Matrix* row_head = m->below;
+    while (row_head != m) {
+        if (row_head->line == x) {
+            Matrix* current = row_head->right;
+            while (current != row_head) {
+                if (current->column == y) {
+                    return current->info;
+                }
+                current = current->right;
             }
-            current_cell = current_cell->right;
+            break;
         }
-        current = current->below;
+        row_head = row_head->below;
     }
-    return 0.0; // Elemento não encontrado
+    return 0.0; // Valor padrão para elementos não encontrados
 }
 
-// Função para definir o valor de um elemento na matriz
 void matrix_setelem(Matrix* m, int x, int y, float elem) {
-    // Percorrer a matriz para encontrar o elemento desejado
-    Matrix* current = m->below;
-    while (current != m) {
-        Matrix* current_cell = current->right;
-        while (current_cell != current) {
-            if (current_cell->line == x && current_cell->column == y) {
-                current_cell->info = elem; // Atualizar o valor do elemento
-                return;
-            }
-            current_cell = current_cell->right;
+    Matrix* row_head = m->below;
+    Matrix* prev_row = m;
+    while (row_head != m && row_head->line < x) {
+        prev_row = row_head;
+        row_head = row_head->below;
+    }
+    
+    if (row_head->line != x) {
+        // A linha x ainda não existe, precisamos criá-la
+        Matrix* new_row = (Matrix*)malloc(sizeof(Matrix));
+        if (new_row == NULL) {
+            perror("Erro na alocação de memória");
+            exit(EXIT_FAILURE);
         }
-        current = current->below;
+        new_row->line = x;
+        new_row->column = -1; // Cabeça da coluna
+        new_row->info = 0.0;
+        new_row->right = new_row;
+        
+        // Inserir nova linha
+        prev_row->below = new_row;
+        new_row->below = row_head;
+        row_head = new_row;
     }
-    // Se o elemento não existir, criar um novo elemento
-    Matrix* new_cell = (Matrix*)malloc(sizeof(Matrix));
-    new_cell->line = x;
-    new_cell->column = y;
-    new_cell->info = elem;
-
-    // Encontrar a posição correta para inserir o novo elemento na linha
-    Matrix* prev = m;
-    Matrix* current_row = m->below;
-    while (current_row != m && current_row->line < x) {
-        prev = current_row;
-        current_row = current_row->below;
+    
+    Matrix* current = row_head->right;
+    Matrix* prev = row_head;
+    
+    while (current != row_head && current->column < y) {
+        prev = current;
+        current = current->right;
     }
-
-    // Conectar o novo elemento na linha
-    prev->below = new_cell;
-    new_cell->below = current_row;
-
-    // Encontrar a posição correta para inserir o novo elemento na coluna
-    prev = m;
-    Matrix* current_col = m->right;
-    while (current_col != m && current_col->column < y) {
-        prev = current_col;
-        current_col = current_col->right;
+    
+    if (current->column == y) {
+        // O elemento (x, y) já existe, atualize-o
+        current->info = elem;
+    } else {
+        // O elemento (x, y) não existe, crie-o
+        Matrix* new_elem = (Matrix*)malloc(sizeof(Matrix));
+        if (new_elem == NULL) {
+            perror("Erro na alocação de memória");
+            exit(EXIT_FAILURE);
+        }
+        new_elem->line = x;
+        new_elem->column = y;
+        new_elem->info = elem;
+        
+        // Inserir novo elemento
+        prev->right = new_elem;
+        new_elem->right = current;
     }
-
-    // Conectar o novo elemento na coluna
-    prev->right = new_cell;
-    new_cell->right = current_col;
 }
 
+void matrix_print(Matrix* m, int numRows, int numCols) {
+    for (int i = 1; i <= numRows; i++) {
+        for (int j = 1; j <= numCols; j++) {
+            float elem = matrix_getelem(m, i, j);
+            printf("%.2f\t", elem);
+        }
+        printf("\n");
+    }
+}
+
+void matrix_destroy(Matrix* m) {
+    Matrix* row_head = m->below;
+    while (row_head != m) {
+        Matrix* current = row_head->right;
+        while (current != row_head) {
+            Matrix* temp = current;
+            current = current->right;
+            free(temp); // Liberar cada célula da matriz
+        }
+        Matrix* temp = row_head;
+        row_head = row_head->below;
+        free(temp); // Liberar a cabeça da linha
+    }
+    free(m); // Liberar a cabeça da matriz
+}
 
 int main() {
-    Matrix* A = matrix_create();
-    matrix_print(A);
-    matrix_destroy(A);
+    Matrix* matrix = matrix_create();
+
+    // Leitura da matriz a partir de um arquivo input.txt
+    FILE* file = fopen("matrix1.txt", "r");
+    if (file == NULL) {
+        perror("Erro na abertura do arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    int numRows, numCols;
+    fscanf(file, "%d %d", &numRows, &numCols);
+
+    int x, y;
+    float elem;
+    while (fscanf(file, "%d %d %f", &x, &y, &elem) == 3) {
+        matrix_setelem(matrix, x, y, elem);
+    }
+    fclose(file);
+
+    // Imprime a matriz no formato desejado, incluindo zeros
+    matrix_print(matrix, numRows, numCols);
+
     return 0;
 }
