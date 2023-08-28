6c144d02c524114cad23bc1b8f2a539388fafc76
Leonardo Braga <braga0425@gmail.com>
2023-08-27 21:14:03 -0300

    Adding the function matrix_add.

--- main.c
+++ main.c
@@ -128,6 +128,67 @@ void matrix_print(Matrix* m) {
     }
 }
 
+Matrix* matrix_add(Matrix* m, Matrix* n) {
+    // Verificar se as matrizes têm o mesmo número de linhas e colunas
+    if (matrix_getelem(m, 1, 1) == 0.0 || matrix_getelem(n, 1, 1) == 0.0) {
+        printf("As matrizes têm tamanhos diferentes.\n");
+        return NULL;
+    }
+
+    // Criar uma matriz resultante com o mesmo tamanho que m e n
+    Matrix* result = matrix_create();
+
+    // Obter o número máximo de linhas e colunas das matrizes m e n
+    int maxLine = -1, maxColumn = -1;
+    Matrix* row_head_m = m->below;
+    Matrix* row_head_n = n->below;
+
+    while (row_head_m != m || row_head_n != n) {
+        int line_m = row_head_m->line;
+        int line_n = row_head_n->line;
+
+        if (line_m > maxLine) {
+            maxLine = line_m;
+        }
+        if (line_n > maxLine) {
+            maxLine = line_n;
+        }
+
+        // Percorra ambas as matrizes para encontrar o número máximo de colunas
+        Matrix* current_m = row_head_m->right;
+        Matrix* current_n = row_head_n->right;
+
+        while (current_m != row_head_m || current_n != row_head_n) {
+            int column_m = current_m->column;
+            int column_n = current_n->column;
+
+            if (column_m > maxColumn) {
+                maxColumn = column_m;
+            }
+            if (column_n > maxColumn) {
+                maxColumn = column_n;
+            }
+
+            current_m = current_m->right;
+            current_n = current_n->right;
+        }
+
+        row_head_m = row_head_m->below;
+        row_head_n = row_head_n->below;
+    }
+
+    // Preencher a matriz resultante com a soma dos elementos correspondentes de m e n
+    for (int i = 1; i <= maxLine; i++) {
+        for (int j = 1; j <= maxColumn; j++) {
+            float elem_m = matrix_getelem(m, i, j);
+            float elem_n = matrix_getelem(n, i, j);
+            matrix_setelem(result, i, j, elem_m + elem_n);
+        }
+    }
+
+    return result;
+}
+
 void matrix_destroy(Matrix* m) {
     Matrix* row_head = m->below;
     while (row_head != m) {
@@ -145,30 +206,64 @@ void matrix_destroy(Matrix* m) {
 }
 
 int main() {
-    Matrix* matrix = matrix_create();
+    Matrix* matrix1 = matrix_create();
+    Matrix* matrix2 = matrix_create();
 
-    // Leitura da matriz a partir de um arquivo input.txt
-    FILE* file = fopen("matrix1.txt", "r");
-    if (file == NULL) {
+    // Leitura da primeira matriz a partir de "matrix1.txt"
+    FILE* file1 = fopen("matrix1.txt", "r");
+    if (file1 == NULL) {
         perror("Erro na abertura do arquivo");
         exit(EXIT_FAILURE);
     }
 
-    int numRows, numCols;
-    fscanf(file, "%d %d", &numRows, &numCols);
+    int numRows1, numCols1;
+    fscanf(file1, "%d %d", &numRows1, &numCols1);
 
-    int x, y;
-    float elem;
-    while (fscanf(file, "%d %d %f", &x, &y, &elem) == 3) {
-        matrix_setelem(matrix, x, y, elem);
+    int x1, y1;
+    float elem1;
+    while (fscanf(file1, "%d %d %f", &x1, &y1, &elem1) == 3) {
+        matrix_setelem(matrix1, x1, y1, elem1);
     }
-    fclose(file);
+    fclose(file1);
 
-    // Imprime a matriz no formato desejado, incluindo zeros
-    matrix_print(matrix);
+    // Imprimir a primeira matriz
+    printf("Matriz 1:\n");
+    matrix_print(matrix1);
+    printf("\n");
 
-    // Destruir a matriz quando não for mais necessária
-    matrix_destroy(matrix);
+    // Leitura da segunda matriz a partir de "matrix2.txt"
+    FILE* file2 = fopen("matrix2.txt", "r");
+    if (file2 == NULL) {
+        perror("Erro na abertura do arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    int numRows2, numCols2;
+    fscanf(file2, "%d %d", &numRows2, &numCols2);
+
+    int x2, y2;
+    float elem2;
+    while (fscanf(file2, "%d %d %f", &x2, &y2, &elem2) == 3) {
+        matrix_setelem(matrix2, x2, y2, elem2);
+    }
+    fclose(file2);
+
+    // Imprimir a segunda matriz
+    printf("Matriz 2:\n");
+    matrix_print(matrix2);
+    printf("\n");
+
+    // Realizar a soma das duas matrizes
+    Matrix* result = matrix_add(matrix1, matrix2);
+
+    // Imprimir o resultado da soma
+    printf("Soma das matrizes:\n");
+    matrix_print(result);
+
+    // Destruir as matrizes quando não forem mais necessárias
+    matrix_destroy(matrix1);
+    matrix_destroy(matrix2);
+    matrix_destroy(result);
 
     return 0;
 }
